f150df3dfc5c4d8609f986fc5c6947dffdafc67c
nel <Recarte11nelson@hotmail.com>
2025-07-06 20:50:38 -0600

    Initial project structure

---
+++ server/db.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+module.exports = sequelize;
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const sequelize = require('./db');
+
+const app = express();
+
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('API running!');
+});
+
+
+
+// Start server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+    try {
+        await sequelize.authenticate();
+        console.log('Database connected successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+});
